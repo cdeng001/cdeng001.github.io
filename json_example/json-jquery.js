32af461e1fd3be992de7b22732ceadffabd09587
cdeng001 <cdeng001@ucr.edu>
2015-07-23 15:19:39 -0700

    ad

--- json_example/json-jquery.js
+++ json_example/json-jquery.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('#getdata-button').live('click', function(){
 		$.getJSON('json-data.php', function(data) {	
 			alert(data); //uncomment this for debug
-			//alert (data.item1+" "+data.item2+" "+data.item3); //further debug
+			alert (data.item1+" "+data.item2+" "+data.item3); //further debug
 			$('#showdata').html("<p>item1="+data.item1+" item2="+data.item2+" item3="+data.item3+"</p>");
 		});
 	});
