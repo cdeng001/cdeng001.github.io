6ea28e5b0efbe9b7a6484768ba50bc4ffe6f6816
cdeng001 <cdeng001@ucr.edu>
2015-07-23 17:20:59 -0700

    s

--- twitter_auth/button_func.js
+++ twitter_auth/button_func.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     //attach a jQuery live event to the button
     $('#test1_button').live('click', function(){
-        $.getJSON('/twitter-proxy.php?url='+encodeURIComponent('statuses/user_timeline.json?screen_name=MikeRogers0&count=2'), function(data) {
+		var url = '/twitter-proxy.php?url='+encodeURIComponent('statuses/user_timeline.json?screen_name=MikeRogers0&count=2');
+        $.getJSON(url, function(data) {
             alert(data); //uncomment this for debug
             //alert (data.item1+" "+data.item2+" "+data.item3); //further debug
             $('#test1').html("<p>hello world</p>");
