632994646db1299489b81bbfc04370c65aa292f6
cdeng001 <cdeng001@ucr.edu>
2015-07-24 17:55:57 -0700

    s

---
+++ twitter_auth/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'qLiTxpzhHNiI1BjJU5xgbPvG1');
+define('CONSUMER_SECRET', 's7lDfWj3gBO5v6j8eVpDScr7UrqZCVbLAaxjbGrZanlP7TCCTm');
+define('OAUTH_CALLBACK', 'http://cdeng001.github.io/twitteroauth/callback.php');
